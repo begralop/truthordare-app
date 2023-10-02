7f776559aaf8c6ca121ecf68ca2dd0ece695472f
Belén <bgralop@epsg.upv.es>
2023-10-02 18:33:05 +0200

    Initial commit

---
+++ app/src/main/java/com/bgralop/truthordare/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.bgralop.truthordare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
