388683ac2fd4e12d478a481e0874c5b8be598a50
Belén Grande <bgralop@epsg.upv.es>
2023-10-04 12:48:23 +0200

    All Fragments are created and working

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,6 @@ plugins {
 }
 
 buildscript {
-    repositories {
-        google()
-    }
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.2")
     }
